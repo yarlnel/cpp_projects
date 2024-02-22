265e9424a8d069f42115731acaa97d9c643edc61
yarlnel <leonbylin3@gmail.com>
2024-02-22 19:06:04 +0700

    Add sem_1 and sem_2 directories

--- cmake-build-debug/CMakeFiles/3.27.0/CMakeCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.27.0/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "F:/CLion 2023.3/bin/mingw/bin/gcc.exe")
+set(CMAKE_C_COMPILER "D:/CLion 2023.3/bin/mingw/bin/gcc.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "13.1.0")
@@ -21,11 +21,11 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "F:/CLion 2023.3/bin/mingw/bin/ar.exe")
-set(CMAKE_C_COMPILER_AR "F:/CLion 2023.3/bin/mingw/bin/gcc-ar.exe")
-set(CMAKE_RANLIB "F:/CLion 2023.3/bin/mingw/bin/ranlib.exe")
-set(CMAKE_C_COMPILER_RANLIB "F:/CLion 2023.3/bin/mingw/bin/gcc-ranlib.exe")
-set(CMAKE_LINKER "F:/CLion 2023.3/bin/mingw/bin/ld.exe")
+set(CMAKE_AR "D:/CLion 2023.3/bin/mingw/bin/ar.exe")
+set(CMAKE_C_COMPILER_AR "D:/CLion 2023.3/bin/mingw/bin/gcc-ar.exe")
+set(CMAKE_RANLIB "D:/CLion 2023.3/bin/mingw/bin/ranlib.exe")
+set(CMAKE_C_COMPILER_RANLIB "D:/CLion 2023.3/bin/mingw/bin/gcc-ranlib.exe")
+set(CMAKE_LINKER "D:/CLion 2023.3/bin/mingw/bin/ld.exe")
 set(CMAKE_MT "")
 set(CMAKE_TAPI "CMAKE_TAPI-NOTFOUND")
 set(CMAKE_COMPILER_IS_GNUCC 1)
@@ -68,7 +68,7 @@ endif()
 
 
 
-set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "F:/CLion 2023.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include;F:/CLion 2023.3/bin/mingw/include;F:/CLion 2023.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include-fixed;F:/CLion 2023.3/bin/mingw/x86_64-w64-mingw32/include")
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "D:/CLion 2023.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include;D:/CLion 2023.3/bin/mingw/include;D:/CLion 2023.3/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include-fixed;D:/CLion 2023.3/bin/mingw/x86_64-w64-mingw32/include")
 set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "")
 set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- cmake-build-debug/CMakeFiles/3.27.0/CMakeRCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.27.0/CMakeRCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_RC_COMPILER "F:/CLion 2023.3/bin/mingw/bin/windres.exe")
+set(CMAKE_RC_COMPILER "D:/CLion 2023.3/bin/mingw/bin/windres.exe")
 set(CMAKE_RC_COMPILER_ARG1 "")
 set(CMAKE_RC_COMPILER_LOADED 1)
 set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
